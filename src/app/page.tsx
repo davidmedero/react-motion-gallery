4fd3b15ae81ab7b5f270bbe82dc866bac1a523bc
David Medero <david.gregory.medero@gmail.com>
2025-06-28 22:48:22 -0400

    debugging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,23 @@
 import styles from "./page.module.css";
-// import ProductImages from "@/components/Thumbnails/ProductImages";
+import ProductImages from "@/components/Thumbnails/ProductImages";
 
 export default function Home() {
 
-  // const thumbnailSliderImages: string[] = [
-  //   'https://react-motion-gallery.s3.us-east-1.amazonaws.com/thumbnail-slider-image-1.jpg',
-  //   'https://react-motion-gallery.s3.us-east-1.amazonaws.com/thumbnail-slider-image-2.jpg',
-  //   'https://react-motion-gallery.s3.us-east-1.amazonaws.com/thumbnail-slider-image-3.jpg',
-  //   'https://react-motion-gallery.s3.us-east-1.amazonaws.com/thumbnail-slider-image-4.jpg',
-  //   'https://react-motion-gallery.s3.us-east-1.amazonaws.com/thumbnail-slider-image-5.jpg',
-  //   'https://react-motion-gallery.s3.us-east-1.amazonaws.com/thumbnail-slider-image-6.jpg',
-  //   'https://react-motion-gallery.s3.us-east-1.amazonaws.com/thumbnail-slider-image-7.jpg',
-  //   'https://react-motion-gallery.s3.us-east-1.amazonaws.com/thumbnail-slider-image-8.jpg'
-  // ]
+  const thumbnailSliderImages: string[] = [
+    'https://react-motion-gallery.s3.us-east-1.amazonaws.com/thumbnail-slider-image-1.jpg',
+    'https://react-motion-gallery.s3.us-east-1.amazonaws.com/thumbnail-slider-image-2.jpg',
+    'https://react-motion-gallery.s3.us-east-1.amazonaws.com/thumbnail-slider-image-3.jpg',
+    'https://react-motion-gallery.s3.us-east-1.amazonaws.com/thumbnail-slider-image-4.jpg',
+    'https://react-motion-gallery.s3.us-east-1.amazonaws.com/thumbnail-slider-image-5.jpg',
+    'https://react-motion-gallery.s3.us-east-1.amazonaws.com/thumbnail-slider-image-6.jpg',
+    'https://react-motion-gallery.s3.us-east-1.amazonaws.com/thumbnail-slider-image-7.jpg',
+    'https://react-motion-gallery.s3.us-east-1.amazonaws.com/thumbnail-slider-image-8.jpg'
+  ]
 
   return (
     <div className={styles.page}>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
-        test
-        {/* <ProductImages urls={thumbnailSliderImages} /> */}
+        <ProductImages urls={thumbnailSliderImages} />
       </div>
     </div>
   );
