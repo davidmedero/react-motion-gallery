368b3551d186fbb892a4ab27d7bd313b20821eaf
David Medero <david.gregory.medero@gmail.com>
2025-06-28 22:51:57 -0400

    debugging

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "react-motion-gallery.s3.us-east-1.amazonaws.com"
+    ],
+  },
 };
 
 export default nextConfig;
